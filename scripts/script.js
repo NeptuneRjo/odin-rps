dffc4b46008019b3ab8100d9206349eb08c24341
NeptuneRjo <rjo9000@outlook.com>
2021-11-20 17:21:52 -0500

    the function to determine round winner is working

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,18 @@
 //  if player wins, display win message to player
 //  player choice is entered with prompt()
 
-// for loop calls playRound 5 times
-// playRound takes both choices and compares them
-// 
+ 
 
 function computerSelection() {
     let comChoice = ["Rock", "Paper", "Scissors"]
     return comChoice[Math.floor(Math.random()*comChoice.length)]
 }
-console.log(computerSelection())
 
-function playerSelection() {
+
+function playerSelection(value) {
     return prompt("Enter your choice: ")
 }
+
 
 // playRound
 // for 5 times
@@ -25,14 +24,13 @@ function playerSelection() {
 // compare
 // return results
 
-// get results from computerSelection and playerSelection
-// compare results with if statements
 let comScore = 0;
 let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection().toLowerCase();
+    computerSelection = computerSelection().toLowerCase();
+    console.log(computerSelection)
 
     if (playerSelection == computerSelection) {
         console.log("Tie game!")
@@ -42,11 +40,14 @@ function playRound(playerSelection, computerSelection) {
         (computerSelection == "paper" && playerSelection == "rock")
     ) {
         console.log("The computer won this round")
-        comScore = comScore++;
+        console.log(`${computerSelection} beats ${playerSelection}!`)
+        comScore = ++comScore;
+
     } else {
         console.log("You won this round")
-        playerScore = playerScore++;
+        console.log(`${playerSelection} beats ${computerSelection}!`)
+        playerScore = ++playerScore;
     }
 }
 
-console.log(playRound())
+console.log(playRound(playerSelection, computerSelection))
